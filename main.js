a3de407babdcc4963887b21757b3fc2a889d873b
Marco Cavalli <cmarcos46@gmail.com>
2020-07-04 19:03:22 +0200

    Added rooms and messages to rooms

--- main.js
+++ main.js
@@ -1,36 +1,79 @@
+/**
+ * Loading Modules
+ */
+
 var express = require('express'),
     app = express(),
     http = require('http').createServer(app),
     io = require('socket.io')(http);
 
+/**
+ * Static files
+ */
+
 app.use(express.static('public'));
+
+/**
+ * Routes defnitions
+ */
 
 app.get('/', function (req, res) {
     res.sendFile(express.static(__dirname + '/public/index.html'));
 });
 
-var roomno = 1;
+/**
+ * 
+ * Manage WebSocket Server
+ */
+
+const rooms = {};
 io.on('connection', function (client) {
 
-    client.on('manageRoom', (data) => {
-        client.join(data.name);
-        let npersons = io.nsps['/'].adapter.rooms[data.name].length;
-        io.sockets.in(data.name).emit('connectToRoom', "You are in room " + data.name + ". There are " + npersons + " users.");
-    });
-
-    client.on('disconnect', () => {
-        console.log("User disconnected!");
-    });
-
-    client.on("leavingRoom", (data) => {
-        let npersons;
-        if (io.nsps['/'].adapter.rooms[data.name] && io.nsps['/'].adapter.rooms[data.name].length > 0) {
-            npersons = io.nsps['/'].adapter.rooms[data.name].length-1;
-            io.sockets.in(data.name).emit('connectToRoom', "You are in room " + data.name + ". There are " + npersons + " users.");
+    client.on("disconnect", () => {
+        if (client.nickname) {
+            leaveRoom(client.roomName, client.nickname);
+            io.sockets.in(client.roomName).emit('user_disconnected', client.nickname);
+            console.log("Client disconnected", client.roomName, client.nickname);
         }
-    });
+    })
+
+    client.on("request_user_list", (roomName) => {
+        client.emit("user_list", rooms[roomName].userList);
+    })
+
+    client.on("login", (data) => {
+        if (rooms[data.roomName] === undefined || (rooms[data.roomName] && rooms[data.roomName].userList.indexOf(data.nickname) < 0) ) {
+            client.join(data.roomName);
+            if(rooms[data.roomName]) {
+                rooms[data.roomName].userList.push(data.nickname);
+            } else {
+                rooms[data.roomName] = {userList: [data.nickname]};
+            }
+            client.nickname = data.nickname;
+            client.roomName = data.roomName;
+            console.info("Client Connected", client.roomName, client.nickname);
+            client.emit("user_list", rooms[client.roomName].userList);
+            io.sockets.in(client.roomName).emit('user_logged', data.nickname);
+        } else {
+                client.emit("nickname-exists");
+        }
+
+        client.on("send_message", (msg) => {
+            io.sockets.in(client.roomName).emit("message", {nickname : client.nickname, message : msg});
+        })
+    })
 })
 
 http.listen(3000, function () {
     console.log('listening on localhost:3000');
 });
+
+/**
+ *  METHODS DEFINITIONS 
+ */
+
+ function leaveRoom(room, nickname) {
+     if(rooms[room]) {
+         rooms[room].userList.splice(rooms[room].userList.indexOf(nickname),1);
+     }
+ }
--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,18 +1,69 @@
 var socket = io(window.location.href);
-var name;
-socket.on("connect", () => {
-    socket.on('connectToRoom', function (data) {
-        $('body').html(data);
-    });
+var sendMsg, receiveMsg;
+var userNickname;
+var roomName;
+var userList = [];
 
-    $("#submit").on('click',() => {
-        name = $('#roomName').val();
-        if (name && name.length > 0) {
-            socket.emit('manageRoom', {name});
-        };
-    });
+$(document).ready(() => {
 
-    $(window).on('beforeunload', () => {
-        socket.emit('leavingRoom',{name});
-    });
+    const loginUser = () => {
+        userNickname = $("#nickname").val();
+        roomName = $("#roomName").val();
+        if (roomName && roomName.length > 1 && userNickname && userNickname.length > 3) {
+            socket = io(window.location.href);
+
+            socket.on('connect', () => {
+
+                socket.on("nickname-exists", () => {
+                    alert("Nickname exists");
+                    socket.close();
+                });
+
+                socket.on("user_list", (list) => {
+                    userList = list;
+                });
+
+                socket.on("user_logged", (nickname) => {
+                    if(nickname == userNickname) {
+                        $('body').html('');
+                    } else {
+                        $('body').append('Utente connesso: ' + nickname + '<br>');
+                        userList.push(nickname);
+                    }
+                });
+
+                socket.on("user_disconnected", (nickname) => {
+                    $('body').append('Utente disconnesso: ' + nickname + '<br>');
+                    userList.splice(userList.indexOf(nickname), 1);
+                });
+
+                socket.emit("login", { nickname: userNickname, roomName: roomName });
+                //socket.emit("request_user_list");
+
+                socket.on("message", (data) => {
+
+                    if (userNickname != data.nickname) {
+                        //messaggio degli altri
+                        receiveMsg(data.nickname, data.message);
+                    }
+                })
+            });
+        }
+    }
+
+    sendMsg = (msg) => {
+        socket.emit("send_message", msg);
+    }
+
+    receiveMsg = (user, msg) => {
+        console.log(user + ": " + msg);
+    }
+
+
+    /*
+    Manage events
+    */
+
+    $("#submit").on('click', loginUser);
+
 });
