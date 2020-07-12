7026c2bf7b52c050907da3434f929f73f5fbd2e0
Marco Cavalli <cmarcos46@gmail.com>
2020-07-12 19:24:44 +0200

    added flames after explosions, concatenated explosions and death after touchign a flame

--- test/assets/js/index.js
+++ test/assets/js/index.js
@@ -22,18 +22,10 @@ var config = {
 };
 
 var game = new Phaser.Game(config);
-var map;
-var tileset;
-var layer;
-var player;
-var player_stats;
-var bombs;
-var cursors;
-var last_pos;
-var bomb_placed;
-var flipFlopBomb;
-var animated;
-var map_bombs;
+var map, tileset, layer, scene;
+var player, player_stats;
+var bombs, flipFlopBomb;
+var cursors, animated; //animated is used to show the right animation with the player sprite
 
 function preload() {
     //1 hard wall, 2 normal wall, 3 grass, 4 shadowed grass,
@@ -58,7 +50,11 @@ function preload() {
 
     this.bombsGroup = this.physics.add.group({
         allowGravity: false
-    })
+    });
+    this.flamesGroup = this.physics.add.group({
+        allowGravity: false
+    });
+    scene = this;
 }
 
 function create() {
@@ -78,50 +74,15 @@ function create() {
 
 
     //ANIMATIONS
-    this.physics.add.collider(player, layer);
-
-
-    this.anims.create({
-        key: 'left',
-        frames: this.anims.generateFrameNumbers('white-bm', { start: 3, end: 5 }),
-        frameRate: 5,
-        repeat: -1
-    });
-
-    this.anims.create({
-        key: 'right',
-        frames: this.anims.generateFrameNumbers('white-bm', { start: 9, end: 11 }),
-        frameRate: 5,
-        repeat: -1
-    });
-
-    this.anims.create({
-        key: 'up',
-        frames: this.anims.generateFrameNumbers('white-bm', { start: 0, end: 2 }),
-        frameRate: 5,
-        repeat: -1
-    });
-
-    this.anims.create({
-        key: 'down',
-        frames: this.anims.generateFrameNumbers('white-bm', { start: 6, end: 8 }),
-        frameRate: 5,
-        repeat: -1
-    });
-
-    this.anims.create({
-        key: 'death',
-        frames: this.anims.generateFrameNumbers('white-bm', { start: 12, end: 17 }),
-        frameRate: 5,
-        repeat: 0
-    });
-
+    playerAnimation();
+    flamesAnimation();
     this.anims.create({
         key: 'bomb-ticking',
         frames: this.anims.generateFrameNumbers('white-bomb', { start: 0, end: 3 }),
         frameRate: 3,
         repeat: 1
     });
+    this.physics.add.collider(player, layer);
 
 
     //CURSORS
@@ -130,114 +91,320 @@ function create() {
     //INITIALIZATIONS VARIABLES
     animated = false;
     bombs = [];
-    player_stats = {};
+    player_stats = { speed: 1, bombs: 1, flames: 12, status: 'alive' };
     flipFlopBomb = false;
 }
 
-var scene;
-
 
 function update() {
-    scene = this;
 
-    player.setVelocity(0, 0);
+    if (player_stats.status === 'alive') {
+        player.setVelocity(0, 0);
 
-    if (cursors.up.isDown) {
-        player.body.velocity.y = -100;
-        if (cursors.left.isDown) {
+        if (cursors.up.isDown) {
+            player.body.velocity.y = -100;
+            if (cursors.left.isDown) {
+                player.body.velocity.x = -100;
+            } else if (cursors.right.isDown) {
+                player.body.velocity.x = +100;
+            }
+
+            player.anims.play('up', true);
+            animated = true;
+
+        } else if (cursors.down.isDown) {
+            player.body.velocity.y = 100;
+            if (cursors.left.isDown) {
+                player.body.velocity.x = -100;
+            } else if (cursors.right.isDown) {
+                player.body.velocity.x = +100;
+            }
+
+            player.anims.play('down', true);
+            animated = true;
+
+        } else if (cursors.left.isDown) {
             player.body.velocity.x = -100;
-        } else if (cursors.right.isDown) {
-            player.body.velocity.x = +100;
+
+            player.anims.play('left', true);
+            animated = true;
+
+        }
+        else if (cursors.right.isDown) {
+            player.body.velocity.x = 100;
+
+            player.anims.play('right', true);
+            animated = true;
+
         }
 
-        player.anims.play('up', true);
-        animated = true;
-
-    } else if (cursors.down.isDown) {
-        player.body.velocity.y = 100;
-        if (cursors.left.isDown) {
-            player.body.velocity.x = -100;
-        } else if (cursors.right.isDown) {
-            player.body.velocity.x = +100;
+        if (!flipFlopBomb && cursors.space.isDown) {
+            place_bomb(player.x, player.y, this);
+            //flipFlopBomb = true;
         }
 
-        player.anims.play('down', true);
-        animated = true;
+        if (flipFlopBomb && cursors.space.isUp) {
+            flipFlopBomb = false;
+        }
 
-    } else if (cursors.left.isDown) {
-        player.body.velocity.x = -100;
+        if (animated && player.body.velocity.x == 0 && player.body.velocity.y == 0) {
+            player.anims.setCurrentFrame(player.anims.currentAnim.frames[1]);
+            player.anims.stop();
+            animated = false;
+        }
 
-        player.anims.play('left', true);
-        animated = true;
-
-    }
-    else if (cursors.right.isDown) {
-        player.body.velocity.x = 100;
-
-        player.anims.play('right', true);
-        animated = true;
-
-    }
-
-    if (!flipFlopBomb && cursors.space.isDown) {
-        place_bomb(player.x, player.y, this);
-        //flipFlopBomb = true;
-    }
-
-    if (flipFlopBomb && cursors.space.isUp) {
-        flipFlopBomb = false;
-    }
-
-    if (animated && player.body.velocity.x == 0 && player.body.velocity.y == 0) {
-        player.anims.setCurrentFrame(player.anims.currentAnim.frames[1]);
-        player.anims.stop();
-        animated = false;
+        if (scene.physics.collide(player, scene.flamesGroup)) {
+            death(player);
+        }
     }
 }
+
+const death = (player) => {
+    player_stats.status = "dead";
+    player.anims.play('death', true);
+    player.once("animationcomplete", () => {
+        setTimeout(() => {
+            player.destroy();
+        }, 1000);
+    });
+};
 
 
 const place_bomb = (x, y, scene) => {
     var bomb;
-    var newX, newY, i, j;
+    var newX, newY, i, j; //i,j are the coordinates of the 16x16 squares of the stage map
     i = Math.floor(Math.floor(x) / 16);
     j = Math.floor(Math.floor(y) / 16);
 
-    newX = Math.floor(x) - (Math.floor(x) % 16) + 8//(i+1) * 16;
-    newY = Math.floor(y) - (Math.floor(y) % 16) + 8//(j+1) * 16;
+    newX = Math.floor(x) - (Math.floor(x) % 16) + 8;
+    newY = Math.floor(y) - (Math.floor(y) % 16) + 8;
 
     if (_.filter(bombs, (b) => Math.floor(b.x / 16) == i && Math.floor(b.y / 16) == j).length == 0) {
-        bomb = scene.bombsGroup.create(-128, -128, 'white-bomb').setOrigin(0,0).disableBody(true,true);
-        //bomb.setPosition(newX,newY);
+        bomb = scene.bombsGroup.create(-128, -128, 'white-bomb').setOrigin(0, 0).disableBody(true, true);
         bomb.x = newX;
         bomb.y = newY;
         bomb.setSize(13, 13).setOffset(2, 2);
+
+        bomb.setInteractive();
+        bomb.setImmovable();
 
         setTimeout(() => { //fixed the bug when the bomb is misplaced on top of an undestructable wall
             if (bombOverlapWall(bomb)) { //bomb misplaced
                 bomb.x -= 8;
                 bomb.y -= 8;
             }
-            bomb.enableBody(false,0,0,true,true);
+            bomb.enableBody(false, 0, 0, true, true);
             bomb.anims.play('bomb-ticking', true);
         }, 50);
 
         bomb.once("animationcomplete", () => {
-            bombs.splice(bombs.indexOf(bomb), 1);
-            bomb.destroy();
+            explosion(bomb);
         });
-        bomb.setInteractive();
-        bomb.setImmovable();
         scene.physics.add.collider(player, bomb);
-        bomb_placed = bomb;
+        scene.physics.add.collider(scene.flamesGroup, bomb, () => {
+            bomb.x += 8;
+            bomb.y += 8;
+            explosion(bomb);
+        });
+
         bombs.push(bomb);
         return bomb;
     }
     return bomb;
 };
 
+const explosion = (bomb) => {
+    bombs.splice(bombs.indexOf(bomb), 1);
+    createFlames(bomb.x, bomb.y);
+    bomb.destroy();
+}
+
 const bombOverlapWall = (bomb) => {
-    return map.getTileAtWorldXY(bomb.x + 1, bomb.y+ 1, layer).index === 1 ||
+    return map.getTileAtWorldXY(bomb.x + 1, bomb.y + 1, layer).index === 1 ||
         map.getTileAtWorldXY(bomb.x + 1, bomb.y + 15, layer).index === 1 ||
         map.getTileAtWorldXY(bomb.x + 15, bomb.y + 1, layer).index === 1 ||
         map.getTileAtWorldXY(bomb.x + 15, bomb.y + 15, layer).index === 1;
+}
+
+const createFlames = (x, y) => {
+    var u = d = l = r = true;
+
+    var flames = [];
+
+    flames.push(scene.flamesGroup.create(x, y, 'white-flame', 5).setOrigin(0, 0));
+    flames[flames.length - 1].animation = "bomb-exploding-center";
+
+    for (let i = 0; i < player_stats.flames; i++) {
+        if (u)
+            u = addFlameUp(flames, x, y, i, player_stats.flames);
+        if (d)
+            d = addFlameDown(flames, x, y, i, player_stats.flames);
+        if (l)
+            l = addFlameLR(flames, x, y, i, player_stats.flames, true);
+        if (r)
+            r = addFlameLR(flames, x, y, i, player_stats.flames, false);
+    }
+
+    _.each(flames, (f) => {
+        f.anims.play(f.animation, 'play');
+        f.once("animationcomplete", () => {
+            f.destroy();
+        });
+    });
+}
+
+const addFlameUp = (flames, x, y, i, length) => {
+    if (map.getTileAtWorldXY(x, y - 16 * (i + 1), layer).index !== 1) {
+        y = y - 16 * (i + 1);
+        if (i == length - 1) {
+            flames.push(scene.flamesGroup.create(x, y, 'white-flame', 20).setOrigin(0, 0));
+            flames[flames.length - 1].animation = "bomb-exploding-up-head";
+        }
+        else {
+            flames.push(scene.flamesGroup.create(x, y, 'white-flame', 10).setOrigin(0, 0));
+            flames[flames.length - 1].animation = "bomb-exploding-ud-body";
+        }
+        return true;
+    } else {
+        return false;
+    }
+}
+
+const addFlameDown = (flames, x, y, i, length) => {
+    if (map.getTileAtWorldXY(x, y + 16 * (i + 1), layer).index !== 1) {
+        y = y + 16 * (i + 1);
+        if (i == length - 1) {
+            flames.push(scene.flamesGroup.create(x, y, 'white-flame', 25).setOrigin(0, 0));
+            flames[flames.length - 1].animation = "bomb-exploding-down-head";
+        }
+        else {
+            flames.push(scene.flamesGroup.create(x, y, 'white-flame', 10).setOrigin(0, 0));
+            flames[flames.length - 1].animation = "bomb-exploding-ud-body";
+        }
+        return true;
+    } else {
+        return false;
+    }
+}
+
+const addFlameLR = (flames, x, y, i, length, isL) => {
+    if (isL) {
+        if (map.getTileAtWorldXY(x - 16 * (i + 1), y, layer).index !== 1) {
+            x = x - 16 * (i + 1);
+            if (i == length - 1) {
+                flames.push(scene.flamesGroup.create(x, y, 'white-flame', 35).setOrigin(0, 0));
+                flames[flames.length - 1].animation = "bomb-exploding-left-head";
+            }
+            else {
+                flames.push(scene.flamesGroup.create(x, y, 'white-flame', 15).setOrigin(0, 0));
+                flames[flames.length - 1].animation = "bomb-exploding-lr-body";
+            }
+            return true;
+        }
+    } else {
+        if (map.getTileAtWorldXY(x + 16 * (i + 1), y, layer).index !== 1) {
+            x = x + 16 * (i + 1);
+            if (i == length - 1) {
+                flames.push(scene.flamesGroup.create(x, y, 'white-flame', 30).setOrigin(0, 0));
+                flames[flames.length - 1].animation = "bomb-exploding-right-head";
+            }
+            else {
+                flames.push(scene.flamesGroup.create(x, y, 'white-flame', 15).setOrigin(0, 0));
+                flames[flames.length - 1].animation = "bomb-exploding-lr-body";
+            }
+            return true;
+        }
+    }
+    return false;
+}
+
+
+
+const playerAnimation = () => {
+    scene.anims.create({
+        key: 'left',
+        frames: scene.anims.generateFrameNumbers('white-bm', { start: 3, end: 5 }),
+        frameRate: 5,
+        repeat: -1
+    });
+
+    scene.anims.create({
+        key: 'right',
+        frames: scene.anims.generateFrameNumbers('white-bm', { start: 9, end: 11 }),
+        frameRate: 5,
+        repeat: -1
+    });
+
+    scene.anims.create({
+        key: 'up',
+        frames: scene.anims.generateFrameNumbers('white-bm', { start: 0, end: 2 }),
+        frameRate: 5,
+        repeat: -1
+    });
+
+    scene.anims.create({
+        key: 'down',
+        frames: scene.anims.generateFrameNumbers('white-bm', { start: 6, end: 8 }),
+        frameRate: 5,
+        repeat: -1
+    });
+
+    scene.anims.create({
+        key: 'death',
+        frames: scene.anims.generateFrameNumbers('white-bm', { start: 12, end: 17 }),
+        frameRate: 5,
+        repeat: 0
+    });
+}
+
+
+const flamesAnimation = () => {
+    scene.anims.create({
+        key: 'bomb-exploding-center',
+        frames: scene.anims.generateFrameNumbers('white-flame', { frames: [5, 6, 7, 8, 9, 8, 7, 6, 5] }),
+        frameRate: 18,
+        repeat: 0
+    });
+
+    scene.anims.create({
+        key: 'bomb-exploding-ud-body',
+        frames: scene.anims.generateFrameNumbers('white-flame', { frames: [10, 11, 12, 13, 14, 13, 12, 11, 10] }),
+        frameRate: 18,
+        repeat: 0
+    });
+
+    scene.anims.create({
+        key: 'bomb-exploding-lr-body',
+        frames: scene.anims.generateFrameNumbers('white-flame', { frames: [15, 16, 17, 18, 19, 18, 17, 16, 15] }),
+        frameRate: 18,
+        repeat: 0
+    });
+
+    scene.anims.create({
+        key: 'bomb-exploding-up-head',
+        frames: scene.anims.generateFrameNumbers('white-flame', { frames: [20, 21, 22, 23, 24, 23, 22, 21, 20] }),
+        frameRate: 18,
+        repeat: 0
+    });
+
+    scene.anims.create({
+        key: 'bomb-exploding-down-head',
+        frames: scene.anims.generateFrameNumbers('white-flame', { frames: [25, 26, 27, 28, 29, 28, 27, 26, 25] }),
+        frameRate: 18,
+        repeat: 0
+    });
+
+    scene.anims.create({
+        key: 'bomb-exploding-right-head',
+        frames: scene.anims.generateFrameNumbers('white-flame', { frames: [30, 31, 32, 33, 34, 33, 32, 31, 30] }),
+        frameRate: 18,
+        repeat: 0
+    });
+
+    scene.anims.create({
+        key: 'bomb-exploding-left-head',
+        frames: scene.anims.generateFrameNumbers('white-flame', { frames: [35, 36, 37, 38, 39, 38, 37, 36, 35] }),
+        frameRate: 18,
+        repeat: 0
+    });
 }
